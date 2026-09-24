a7a217ac540ebe03388293c8f1271def12278c59
rylieam <ryliemo2006@gmail.com>
2026-09-24 13:33:07 -0400

    added deposit button function

--- lab5/script.js
+++ lab5/script.js
@@ -27,3 +27,28 @@ function withdraw() {
 
     }
 }
+function deposit() {
+    money = money + depositAmount - damageAmount;
+
+    const healthText = document.getElementById("money-display");
+    const statusText = document.getElementById("status-message");
+
+    if(money > 0)
+    {
+        healthText.innerText = money;
+        statusText.innerText = "You're on your way to becoming a millionaire!";
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "You went bankrupt!";
+        statusText.style.color = "#f9331d";
+        statusText.style.fontWeight = "bold";
+        
+        document.body.style.backgroundColor = "#610a00";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Bankrupt";
+
+    }
+}
