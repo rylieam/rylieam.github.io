6a0b6a5e4440c1ba59d408b555fae7805934e916
rylieam <ryliemo2006@gmail.com>
2026-09-24 14:04:30 -0400

    trieed it again

--- lab5/script.js
+++ lab5/script.js
@@ -35,10 +35,10 @@ function deposit() {
 
         healthText.innerText = > 0;
         statusText.innerText = "You're on your way to becoming a millionaire!";
-        statusText.style.color = "#ffffff";
+        statusText.style.color = "#008b8b";
         statusText.style.fontWeight = "bold";
         
-        document.body.style.backgroundColor = "#008b8b";
+        document.body.style.backgroundColor = "#ffffff";
 
         document.querySelector("button").disabled = false;
         document.querySelector("button").innerText = "Deposit";
