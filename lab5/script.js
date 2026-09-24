10333badcc2147ec8efad20ae31f5fab19e96576
rylieam <ryliemo2006@gmail.com>
2026-09-24 13:31:35 -0400

    Initial Commits lab 5

---
+++ lab5/script.js
@@ -0,0 +1,29 @@
+let money = 0;
+const withdrawAmount = 20;
+const depositAmount = 20;
+
+function withdraw() {
+    money = money + depositAmount - damageAmount;
+
+    const healthText = document.getElementById("money-display");
+    const statusText = document.getElementById("status-message");
+
+    if(money > 0)
+    {
+        healthText.innerText = money;
+        statusText.innerText = "You're on your way to becoming a millionaire!";
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "You went bankrupt!";
+        statusText.style.color = "#f9331d";
+        statusText.style.fontWeight = "bold";
+        
+        document.body.style.backgroundColor = "#610a00";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Bankrupt";
+
+    }
+}
